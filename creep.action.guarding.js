6be33ce2a791311795ecd43bf3f12e467d961166
Perrytheplatypus43 <alexander.i.bakalov@gmail.com>
2017-03-23 18:58:05 -0700

    Update creep.action.guarding.js

--- creep.action.guarding.js
+++ creep.action.guarding.js
@@ -30,7 +30,7 @@ action.work = function(creep){
 }*/
 action.onAssignment = function(creep, target) {
     console.log('on Assignment working');
-    if( room.find(FIND_SPAWN) = false ){
+    if( !room.find(FIND_SPAWN).lenght ){
         creep.say('We Won!!!!!!Yahoo!!!Congradulations!!!!');
     }else{
          if( SAY_ASSIGNMENT ) creep.say(String.fromCharCode(9929), SAY_PUBLIC);
